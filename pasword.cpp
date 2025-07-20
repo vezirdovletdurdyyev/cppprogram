7382185a25f1b849d4baa40b2cadcf74d1c1138d
Vezir Dovletdurdyyev <vezirdovletdurdyyevofficial@gmail.com>
2025-07-20 19:32:45 +0800

    pasword

---
+++ pasword.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int pasword = 2308;
+    int guess = 0;
+
+    while (  guess != pasword) {
+        cout << "pasword:";
+    cin >> guess;
+     }
+     if (guess  == pasword )
+     cout <<  "caman beibi";
+    int pause;
+    cin >> pause;
+    return 0;
+
+}
