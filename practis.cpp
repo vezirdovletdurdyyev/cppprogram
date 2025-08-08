6602e9e3b8ed4d08912893778fc21d0a81027edc
Vezir Dovletdurdyyev <vezirdovletdurdyyevofficial@gmail.com>
2025-08-08 11:04:37 +0800

    practis1

---
+++ practis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+   int a = 1;
+   int b = 2;
+    int temp = a;
+    a = b;
+    b = temp;
+   cout << a ;
+    int pause;
+    cin >> pause;
+    return 0;
+
+}
