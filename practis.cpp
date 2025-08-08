b79915102f26803d38945dd734f9f40944481e94
Vezir Dovletdurdyyev <vezirdovletdurdyyevofficial@gmail.com>
2025-08-08 17:54:10 +0800

    practis6

--- practis.cpp
+++ practis.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <cmath>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main ()
 {
     
-    const double pi = 3.14;
-    double radius;
-    cout << "Radius:";
-    cin >> radius;
-  
-    double S = pi * pow(radius ,2);
-    cout << S;
-
+    const short maxValue = 6;
+    const short minValue = 1;
+    srand (time (0));
+    short number1 = ((rand()% (maxValue - minValue +1 ))+minValue);
+    short number2 = ((rand()% (maxValue - minValue +1 ))+minValue);
+    cout << number1 << endl;
+    cout << number2;
     int pause;
     cin >> pause;
     return 0;
